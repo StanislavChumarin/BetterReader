104de1ea93d3de8ce3e93a2507c84d0079ea889f
stanislavchumarin <stanislav.chumarin@gmail.com>
2012-11-30 02:00:03 +0200

    Trying to use sliding menu

---
+++ BetterReaderG/src/com/staschum/Colorfragment.java
@@ -0,0 +1,11 @@
+package com.staschum;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stanislavchumarin
+ * Date: 30.11.12
+ * Time: 01:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Colorfragment {
+}
