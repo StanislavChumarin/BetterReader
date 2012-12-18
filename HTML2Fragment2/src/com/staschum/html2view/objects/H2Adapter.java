b5299b4df1bc2b0dbf704c2c15e0e198ebccda21
schumarin <schumarin@cogniance.com>
2012-12-18 18:35:07 +0200

    New objects to hold json and incapsulate conversion logic.

---
+++ HTML2Fragment2/src/com/staschum/html2view/objects/H2Adapter.java
@@ -0,0 +1,11 @@
+package com.staschum.html2view.objects;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: schumarin
+ * Date: 18.12.12
+ * Time: 18:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class H2Adapter {
+}
---
+++ HTML2Fragment2/src/com/staschum/html2view/objects/H2Screen.java
@@ -0,0 +1,11 @@
+package com.staschum.html2view.objects;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: schumarin
+ * Date: 18.12.12
+ * Time: 18:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class H2Screen {
+}
